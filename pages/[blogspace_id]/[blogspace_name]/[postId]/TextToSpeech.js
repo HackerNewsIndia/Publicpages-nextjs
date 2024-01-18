accde70060414e536ab303b20cb48dcc1d03539a
Dinesh <dinesh.melapundi@gmail.com>
2024-01-18 20:28:34 +0530

    added icons to the play, pause and stop buttons

--- pages/[blogspace_id]/[blogspace_name]/[postId]/TextToSpeech.js
+++ pages/[blogspace_id]/[blogspace_name]/[postId]/TextToSpeech.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faPause, faStop } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlay,
+  faPause,
+  faStop,
+  faVolumeHigh,
+} from "@fortawesome/free-solid-svg-icons";
 import Highlighter from "react-highlight-words";
-import AudioPlayer from "./AudioPlayer";
+import AudioPlayer from "./audioPlayer";
 import "../../../../styles/text_highlight.module.css";
 
 const TextToSpeech = ({ text, setCurrentWord, currentWord, isActive }) => {
-  const [isPaused, setIsPaused] = useState(false);
+  const [IsTextToSpeechActive, setIsTextToSpeechActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
   const [utterance, setUtterance] = useState(null);
   const [selectedVoice, setSelectedVoice] = useState(null);
-  const [ttsAudioData, setTTSAudioData] = useState(null);
+  // const [ttsAudioData, setTTSAudioData] = useState(null);
 
-  const u = new SpeechSynthesisUtterance(text);
+  // const u = new SpeechSynthesisUtterance(text);
 
-  const saveTTSData = (audioData) => {
-    setTTSAudioData(audioData);
-  };
+  console.log("is button paused:", isPaused);
+
+  // const saveTTSData = (audioData) => {
+  //   setTTSAudioData(audioData);
+  // };
 
   useEffect(() => {
     const synth = window.speechSynthesis;
@@ -24,6 +32,7 @@ const TextToSpeech = ({ text, setCurrentWord, currentWord, isActive }) => {
       const voices = synth.getVoices();
       if (voices.length > 0) {
         setSelectedVoice(voices[0]);
+        const u = new SpeechSynthesisUtterance(text);
         u.voice = voices[0];
         setUtterance(u);
       }
@@ -43,7 +52,7 @@ const TextToSpeech = ({ text, setCurrentWord, currentWord, isActive }) => {
         if (event.name === "word" && event.charIndex !== undefined) {
           const word = text.slice(
             event.charIndex,
-            event.charIndex + event.charLength,
+            event.charIndex + event.charLength
           );
           setCurrentWord(word);
         }
@@ -59,41 +68,41 @@ const TextToSpeech = ({ text, setCurrentWord, currentWord, isActive }) => {
 
   const handlePlay = () => {
     const synth = window.speechSynthesis;
+    console.log("Handle Play called");
 
     if (isPaused) {
-      synth.resume();
-    } else {
-      if (selectedVoice) {
-        u.voice = selectedVoice;
+      if (synth.paused) {
+        synth.resume();
+      } else {
+        const u = new SpeechSynthesisUtterance(text);
+        synth.speak(u);
       }
-      synth.speak(u);
-      const audioData = synth.speak(u);
-      saveTTSData(audioData);
-      sendTTSDataToServer(audioData);
+    } else {
+      synth.pause();
     }
 
-    setIsPaused(false);
+    setIsPaused(!isPaused);
   };
 
-  const sendTTSDataToServer = (audioData) => {
-    const formData = new FormData();
-    formData.append("ttsAudio", audioData);
+  // const sendTTSDataToServer = (audioData) => {
+  //   const formData = new FormData();
+  //   formData.append("ttsAudio", audioData);
 
-    fetch("http://127.0.0.1:5001/upload-audio", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log("TTS audio sent to the server successfully.");
-        } else {
-          console.error("Failed to send TTS audio to the server.");
-        }
-      })
-      .catch((error) => {
-        console.error("Error sending TTS audio:", error);
-      });
-  };
+  //   fetch("http://127.0.0.1:5001/upload-audio", {
+  //     method: "POST",
+  //     body: formData,
+  //   })
+  //     .then((response) => {
+  //       if (response.ok) {
+  //         console.log("TTS audio sent to the server successfully.");
+  //       } else {
+  //         console.error("Failed to send TTS audio to the server.");
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error sending TTS audio:", error);
+  //     });
+  // };
 
   const handlePause = () => {
     const synth = window.speechSynthesis;
@@ -104,47 +113,52 @@ const TextToSpeech = ({ text, setCurrentWord, currentWord, isActive }) => {
   const handleStop = () => {
     const synth = window.speechSynthesis;
     synth.cancel();
-    setIsPaused(false);
+    setIsPaused(true);
+    setUtterance(new SpeechSynthesisUtterance(text));
   };
 
   return (
     <div>
+      {/* <FontAwesomeIcon icon={faVolume} /> */}
+
       <div
-        className={`flex space-x-4 fixed ${isActive ? "w-1/6" : ""} ${
-          isActive ? "space-x-6" : "space-x-4"
-        }  bottom-4 right-1 border-2 border-black bg-white bg-opacity-50  rounded-md px-2 py-2`}
+        className={`flex space-x-4 fixed 
+        bottom-1 right-1 px-2 py-2`}
+        onMouseEnter={() => setIsTextToSpeechActive(true)}
+        onMouseLeave={() => setIsTextToSpeechActive(false)}
       >
-        <button
-          className={`${isActive ? "px-3 py-2" : "px-2 py-1"} rounded-md ${
-            isPaused ? "bg-yellow-500" : "bg-blue-500"
-          } text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50`}
-          onClick={handlePlay}
-        >
-          <FontAwesomeIcon icon={isPaused ? faPlay : faPause} />
-        </button>
-        <button
-          className={`${
-            isActive ? "px-3 py-2" : "px-2 py-1"
-          } rounded-md bg-yellow-500 text-white hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-opacity-50`}
-          onClick={handlePause}
-        >
-          <FontAwesomeIcon icon={faPause} />
-        </button>
-        <button
-          className={`${
-            isActive ? "px-3 py-1" : "px-2 py-1"
-          } rounded-md bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50`}
-          onClick={handleStop}
-        >
-          <FontAwesomeIcon icon={faStop} />
-        </button>
+        <div className="justify-center p-auto text-slate-900 text-2xl ">
+          <FontAwesomeIcon icon={faVolumeHigh} />{" "}
+        </div>
+        {IsTextToSpeechActive ? (
+          <div className="justify-center space-x-4">
+            <button
+              className={`${isActive ? "px-2 py-1" : "px-2 py-1"} rounded-md ${
+                isPaused ? "bg-blue-500" : "bg-yellow-500"
+              } text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50`}
+              onClick={isPaused ? handlePlay : handlePause}
+            >
+              {isPaused ? (
+                <FontAwesomeIcon icon={faPlay} />
+              ) : (
+                <FontAwesomeIcon icon={faPause} />
+              )}
+            </button>
+            <button
+              className={`${
+                isActive ? "px-2 py-1" : "px-2 py-1"
+              } rounded-md  bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50 ${
+                isPaused ? "opacity-0.7 cursor-not-allowed" : ""
+              }`}
+              onClick={handleStop}
+              style={{ opacity: isPaused ? 0.7 : 1 }}
+              disabled={isPaused}
+            >
+              <FontAwesomeIcon icon={faStop} />
+            </button>
+          </div>
+        ) : null}
       </div>
-      <Highlighter
-        highlightClassName="highlighted-text"
-        searchWords={[currentWord]}
-        autoEscape={true}
-        textToHighlight={text}
-      />
     </div>
   );
 };

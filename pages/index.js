490a4d5cb1f85f9a9a434118c98dab1c07a3a43d
Dinesh <dinesh.melapundi@gmail.com>
2024-01-19 15:59:44 +0530

    change in importing BlogProvider component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import Home from "../components/home";
-import { BlogProvider } from "./blogcontext";
+import BlogProvider from "./blogcontext";
 import Header from "../components/header";
 
 export default function HomePage() {

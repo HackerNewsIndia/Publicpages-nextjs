c79ccaf6d9239e2ed0956907d8803c8cb52d3c7b
Dinesh <dinesh.melapundi@gmail.com>
2024-01-29 20:42:05 +0530

    added footer element

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import Home from "../components/home";
 import BlogProvider from "./blogcontext";
 import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function HomePage() {
   return (
     <BlogProvider>
       <Header />
       <Home />
+      <Footer />
     </BlogProvider>
   );
 }

521f8bd86bde2e594e99dd1da4ae023712bb77b9
athishsreeram <athishsreeram007@gmail.com>
2024-02-07 23:00:48 -0500

    Added generateMetadata

--- pages/[blogspace_id]/[blogspace_name]/[postId]/post.js
+++ pages/[blogspace_id]/[blogspace_name]/[postId]/post.js
@@ -15,7 +15,7 @@ import Postsentiment from "./postsentiment";
 import Sharepost from "./sharepost";
 import Footer from "../../../../components/footer";
 
-const Post = () => {
+const Post = ({ params, searchParams }) => {
   const router = useRouter();
   const { blogspace_id, postId } = router.query || {};
   const [currentWord, setCurrentWord] = useState("");
@@ -92,9 +92,7 @@ const Post = () => {
     router.back();
   };
 
-  if (!post) {
-    return <div>Loading...</div>;
-  }
+  
 
   const stripMarkdown = (md) => {
     // Remove headers
@@ -320,5 +318,25 @@ const Post = () => {
     </div>
   );
 };
+
+export async function generateMetadata({ params, searchParams }, parent) {
+   const { blogspace_id, postId } = params;
+
+   const response = await fetch(
+     `https://diaryblogapi2.onrender.com/api/companies/${blogspace_id}/posts/${postId}`
+   );
+   const post = await response.json();
+
+   const previousImages = (await parent).openGraph?.images || [];
+   console.log("metacalled");
+
+   return {
+     title: post.title,
+     description: post.description,
+     openGraph: {
+       images: [post.imageUrl, ...previousImages],
+     },
+   }
+}
 
 export default Post;

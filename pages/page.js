f3373ab2ae6040ea6ca2a7fca86e7cac665111a4
athishsreeram <athishsreeram007@gmail.com>
2024-02-09 18:53:29 -0500

    removed Metadata

--- pages/page.js
+++ pages/page.js
@@ -4,37 +4,10 @@ import Home from "../components/home";
 import Head from "next/head";
 
 
-export const metadata = {
-  title: 'Acme',
-  openGraph: {
-    title: 'Acme',
-    description: 'Acme is a...',
-  },
-}
-
-
 export default function HomeApp() {
 
   
   return (
-     <>
-     <Head>
-         <title>Diary-Blog1</title>
-         <meta property="og:title" content="Diary-Blog1" />
-         <meta property="og:description" content="Writing and publishing articles or posts online, sharing thoughts, opinions, and expertise on various topics to engage with an audience or community" />
-         <meta property="og:image" content="" />
-         <meta property="og:url" content="https://universe-nextjs.onrender.com/" />
-         <meta property="og:locale" content="en_US" />
-         <meta property="og:image:url" content="https://universe-nextjs.onrender.com/_next/static/media/logo2.82936ace.svg" />
-         <meta property="og:image:width" content="800" />
-         <meta property="og:image:height" content="600" />
-         <meta property="og:image:url" content="https://universe-nextjs.onrender.com/_next/static/media/logo2.82936ace.svg" />
-         <meta property="og:image:width" content="1800" />
-         <meta property="og:image:height" content="1600" />
-         <meta property="og:image:alt" content="Diary Blog" />
-         <meta property="og:type" content="website" />
-     </Head>
     <Home />
-    </>
     );
 }

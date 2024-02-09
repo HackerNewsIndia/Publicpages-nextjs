a56324c0bc31ab23b5a2f44aab26aefc59359ed9
athishsreeram <athishsreeram007@gmail.com>
2024-02-09 16:59:14 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,20 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import Head from "next/head";
 
+export const metadata = {
+  title: 'Acme2',
+  openGraph: {
+    title: 'Acme2',
+    description: 'Acme2 is a...',
+  },
+}
+
 export default function HomePage() {
   return (
      <>
       <Head>
-        <title>DiaryBlog</title>
-        <meta property="og:title" content="DiaryBlog" />
+        <title>DiaryBlog2</title>
+        <meta property="og:title" content="Diary-Blog2" />
         <meta property="og:description" content="Writing and publishing articles or posts online, sharing thoughts, opinions, and expertise on various topics to engage with an audience or community" />
         <meta property="og:image" content="https://universe-nextjs.onrender.com/_next/static/media/logo2.82936ace.svg" />
     </Head>

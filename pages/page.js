4c77da6aaddbf895a11b1687552b2e7590aa436b
athishsreeram <athishsreeram007@gmail.com>
2024-02-08 23:52:54 -0500

    added  <>      <Head>          <title>DiaryBlog</title>          <meta property="og:title" content="DiaryBlog" />          <meta property="og:description" content="Writing and publishing articles or posts online, sharing thoughts, opinions, and expertise on various topics to engage with an audience or community" />          <meta property="og:image" content="https://universe-nextjs.onrender.com/_next/static/media/logo2.82936ace.svg" />      </Head>

--- pages/page.js
+++ pages/page.js
@@ -1,7 +1,21 @@
 import Image from "next/image";
 import React from "react";
 import Home from "../components/home";
+import Head from "next/head";
+
 
 export default function HomeApp() {
-  return <Home />;
+
+  
+  return (
+     <>
+     <Head>
+         <title>DiaryBlog</title>
+         <meta property="og:title" content="DiaryBlog" />
+         <meta property="og:description" content="Writing and publishing articles or posts online, sharing thoughts, opinions, and expertise on various topics to engage with an audience or community" />
+         <meta property="og:image" content="https://universe-nextjs.onrender.com/_next/static/media/logo2.82936ace.svg" />
+     </Head>
+    <Home />
+    </>
+    );
 }

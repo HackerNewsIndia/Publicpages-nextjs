a2651bc8d85c095bca1849c77c530026a8ce1777
athishsreeram <athishsreeram007@gmail.com>
2024-02-10 19:15:22 -0500

    Update blogcontext.js

--- pages/blogcontext.js
+++ pages/blogcontext.js
@@ -16,11 +16,24 @@ function BlogProvider({ children }) {
       <Head>
         <title>DiaryBlog All Post</title>
         <meta property="og:title" content="DiaryBlog All Post" />
-        <meta property="og:description" content="Writing and publishing articles or posts online, sharing thoughts, opinions, and expertise on various topics to engage with an audience or community" />
-        <meta
-          property="og:image"
-          content="https://universe-nextjs.onrender.com/DBlogo.png"
-        />
+        
+
+        <!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://universe-nextjs.onrender.com/">
+<meta property="og:type" content="website">
+<meta property="og:title" content="DiaryBlog All Post">
+<meta property="og:description" content="Writing and publishing articles or posts online, sharing thoughts, opinions, and expertise on various topics to engage with an audience or community">
+<meta property="og:image" content="https://universe-nextjs.onrender.com/DBlogo.png">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="universe-nextjs.onrender.com">
+<meta property="twitter:url" content="https://universe-nextjs.onrender.com/">
+<meta name="twitter:title" content="DiaryBlog All Post">
+<meta name="twitter:description" content="Writing and publishing articles or posts online, sharing thoughts, opinions, and expertise on various topics to engage with an audience or community">
+<meta name="twitter:image" content="https://universe-nextjs.onrender.com/DBlogo.png">
+
+            
     </Head>
     <BlogContext.Provider
       value={{ blogData, setBlogData, selectedBlog, setSelectedBlog }}

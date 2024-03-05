db3f8448e449cff823ebdcac084da819863c9f48
athishsreeram <athishsreeram007@gmail.com>
2024-03-04 19:01:05 -0500

    Meta https://diaryblog.connectingpeopletech.com/DBlogo.png

--- pages/diaryblogspace.js
+++ pages/diaryblogspace.js
@@ -398,7 +398,7 @@ const PublicBlogSpace = () => {
         />
         <meta
           property="og:image"
-          content="https://https://diaryblog.connectingpeopletech.com/DBlogo.png"
+          content="https://diaryblog.connectingpeopletech.com/DBlogo.png"
         />
       </Head>
       <div>
